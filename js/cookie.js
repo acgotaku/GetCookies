f7a7bf82c2eaec4e892d0dd2e6728663d670f9a7
acgotaku <acgotaku311@gmail.com>
2014-08-20 20:32:51 +0800

    change listen

--- js/cookie.js
+++ js/cookie.js
@@ -8,19 +8,19 @@ function onload(func) {
 }
 
 onload(function() { 
-    var export_btn = document.querySelector("[data-toggle='get-cookie']");
-    if(export_btn){
-        console.log("Cookies Helper Start");
-        var site = export_btn.getAttribute("data-site");
-        var name = export_btn.getAttribute("data-name");
-        var domain = export_btn.getAttribute("data-domain");
-        export_btn.addEventListener('click', function() {
+        window.addEventListener('click', function(event) {
+            var export_btn=event.target;
+            if(export_btn.getAttribute("data-toggle")=="get-cookie"){
+             var site = export_btn.getAttribute("data-site");
+            var name = export_btn.getAttribute("data-name");
+            var domain = export_btn.getAttribute("data-domain");
             var port = chrome.runtime.connect({name: "get_cookie"});
             port.postMessage({"do": "get_cookie", "site": site, "name": name,"domain":domain});
             port.onMessage.addListener(function(msg) {
                 export_btn.setAttribute("data-cookie",JSON.stringify(msg));
             });
+            }
 
         }, false);
-    }
+    
 });
